298decd036a4a41698a6e4814ab0a8e33171d063
kl <kl@intentus.de>
2024-12-17 09:53:51 +0100

    Erstes Modul eingabe Zahl

---
+++ Wochenaufgabe5/helpers.py
@@ -0,0 +1,6 @@
+def eingabe_zahl(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Ungültige Eingabe. Bitte eine gültige Zahl eingeben!")
