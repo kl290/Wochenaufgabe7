40b13f47e0b87ac9d123200e272722ea959042ed
kl <kl@intentus.de>
2024-12-17 09:50:39 +0100

    Erstes Modul eingabe Zahl bei Aufgabe 3

--- Wochenaufgabe5/Aufgabe3.py
+++ Wochenaufgabe5/Aufgabe3.py
@@ -1,15 +1,10 @@
-def zahl_eingeben(prompt):
-    while True:
-        try:
-            return float(input(prompt))
-        except ValueError:
-            print("Ungültige Eingabe. Bitte eine gültige Zahl eingeben!")
+from helpers import eingabe_zahl
 
 
 print("Dieser Rechner berechnet die Summe, Differenz, Multiplikation und Division von 2 Zahlen.")
 
-Zahl1 = zahl_eingeben("Gib die erste Zahl ein: ")
-Zahl2 = zahl_eingeben("Gib die zweite Zahl ein: ")
+Zahl1 = eingabe_zahl("Gib die erste Zahl ein: ")
+Zahl2 = eingabe_zahl("Gib die zweite Zahl ein: ")
 
 print(f"{Zahl1} + {Zahl2} = {Zahl1 + Zahl2}")
 print(f"{Zahl1} - {Zahl2} = {Zahl1 - Zahl2}")
