c09869f78a23495448a0e9f57c098f1c0f5e9525
kl <kl@intentus.de>
2025-01-10 14:13:33 +0100

    Test zur Funktion zahl_eingabe

--- Unittest/test_eingabe.py
+++ Unittest/test_eingabe.py
@@ -1,7 +1,7 @@
 import unittest
 from unittest.mock import patch
 
-from eingabe import eingabe_zahl
+from zahl_eingabe import eingabe_zahl
 
 
 class TestEingabeZahl(unittest.TestCase):
@@ -11,7 +11,7 @@ class TestEingabeZahl(unittest.TestCase):
         result = eingabe_zahl("Gib eine Zahl ein: ")
         self.assertEqual(result, 5.0)
 
-    @patch('builtins.input', side_effect=['abc', '3.14'])
+    @patch('builtins.input', side_effect=['a', '3.14'])
     def test_ungueltige_eingabe(self, mock_input):
         result = eingabe_zahl("Gib eine Zahl ein: ")
         self.assertEqual(result, 3.14)
@@ -19,3 +19,4 @@ class TestEingabeZahl(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
