9f3ce0fc6fdbf128f1fa967d327b196bcc698a42
kl <kl@intentus.de>
2025-01-15 08:34:35 +0100

    Erweiterung des Tests

--- Unittest/test_eingabe.py
+++ Unittest/test_eingabe.py
@@ -6,17 +6,41 @@ from zahl_eingabe import eingabe_zahl
 
 class TestEingabeZahl(unittest.TestCase):
 
-    @patch('builtins.input', return_value='5')
-    def test_gueltige_eingabe(self, mock_input):
-        result = eingabe_zahl("Gib eine Zahl ein: ")
-        self.assertEqual(result, 5.0)
+    @patch('builtins.input', side_effect=['5.0'])
+    def test_float_to_int(self, mock_input):
+        result = eingabe_zahl("Geben sie eine Zahl ein: ")
+        self.assertEqual(result, 5)
 
-    @patch('builtins.input', side_effect=['a', '3.14'])
-    def test_ungueltige_eingabe(self, mock_input):
-        result = eingabe_zahl("Gib eine Zahl ein: ")
-        self.assertEqual(result, 3.14)
+    @patch('builtins.input' , side_effect=['3.5'])
+    def test_float(self, mock_input):
+        result = eingabe_zahl("Geben Sie eine zahl ein: ")
+        self.assertEqual(result, 3.5)
+
+    @patch('builtins.input', side_effect=['-2'])
+    def test_int(self, mock_input):
+        result = eingabe_zahl("Geben Sie eine Zahl ein: ")
+        self.assertEqual(result, -2)
+
+    @patch('builtins.input', side_effect=['100'])
+    def test_mit_min_value(self, mock_input):
+        result = eingabe_zahl("Geben Sie eine Zahl ein: ", min_val=50)
+        self.assertEqual(result, 100)
+
+    @patch('builtins.input', side_effect=['20'])
+    def test_mit_min_value_error(self, mock_input):
+        result = eingabe_zahl("Geben Sie eine Zahl ein: ", min_val=10)
+        self.assertEqual(result, 20)
+
+    @patch('builtins.input', side_effect=['200'])
+    def test_mit_max_value(self, mock_input):
+        result = eingabe_zahl("Geben Sie eine Zahl ein: ", max_val=250)
+        self.assertEqual(result, 200)
+
+    @patch('builtins.input', side_effect=['50'])
+    def test_mit_max_value_error(self, mock_input):
+        result = eingabe_zahl("Geben Sie eine zahl ein: ", max_val=100)
+        self.assertEqual(result, 50)
 
 
 if __name__ == '__main__':
     unittest.main()
-
